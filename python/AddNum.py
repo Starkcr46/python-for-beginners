750c86e504814fc374058082ab8efc61f3eb9b94
Starkcr46 <starkcr39@gmail.com>
2025-04-08 04:39:35 -0500

    Create AddNum.py

---
+++ python/AddNum.py
@@ -0,0 +1,11 @@
+#Program for adding two numbers
+#Created on : 04-April-2025
+#Created by : Chris Stark
+
+print('This is a program to add two numbers')
+x = input('Key in the first number : ')
+y = input('Key in the second number: ')
+x = int(x)
+y = int(y)
+z = x + y
+print('The value of Sum : ', z)
