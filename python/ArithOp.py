503cc8cbcb584a5746270db0da67a6147d78548b
Starkcr46 <starkcr39@gmail.com>
2025-04-11 06:42:15 -0500

    test arithmetic ops

--- python/ArithOp.py
+++ python/ArithOp.py
@@ -3,6 +3,22 @@
 #Created By: Chris Stark
 
 print('This program demonstrates the various arithmentic operations using integers')
+
+def addNum(int1, int2):
+    return int1 + int2
+def subNum(int1, int2):
+    return int1 - int2
+def multNum(int1, int2):
+    return int1 * int2
+def divNum(int1, int2):
+    return int1 / int2
+def modNum(int1, int2):
+    return int1 % int2
+def floorNum(int1, int2):
+    return int1 // int2
+def expNum(int1, int2):
+    return int1 ** int2
+
 try :
     integerOne = int(input('input int #1 :'))
     integerTwo = int(input('input int #2 :'))
@@ -11,13 +27,13 @@ except :
     print('Error: inputs must be integers')
 
 else :
-    add = integerOne + integerTwo
-    sub = integerOne - integerTwo
-    mult = integerOne * integerTwo
-    div = float(integerOne / integerTwo)
-    mod = integerOne % integerTwo
-    floor = integerOne // integerTwo
-    exp = integerOne ** integerTwo
+    add = addNum(integerOne, integerTwo)
+    sub = subNum(integerOne, integerTwo)
+    mult = multNum(integerOne, integerTwo)
+    div = divNum(integerOne, integerTwo)
+    mod = modNum(integerOne, integerTwo)
+    floor = floorNum(integerOne, integerTwo)
+    exp = expNum(integerOne, integerTwo)
     print('Addition :', add)
     print('Subtraction :', sub)
     print('Multiply :', mult)
@@ -25,3 +41,5 @@ else :
     print('Modulus :', mod)
     print('Floor Division :', floor)
     print('Exponent :', exp)
+
+
--- python/tests/testArithOp.py
+++ python/tests/testArithOp.py
@@ -1,2 +1,33 @@
-import unittest
-import ArithOp
+import unittest as ut
+import importlib.util
+import sys
+import os
+
+def import_from_path(module_name, module_path):
+        spec = importlib.util.spec_from_file_location(module_name, module_path)
+        module = importlib.util.module_from_spec(spec)
+        sys.modules[module_name] = module
+        spec.loader.exec_module(module)
+        return module
+
+module_path = os.path.join('C:/code/python', 'ArithOp.py')
+ao = import_from_path('ArithOp', module_path)
+
+class TestArithmetic(ut.TestCase):
+     def test_add(self):
+        self.assertEqual(ao.addNum(2, 3), 5)
+     def test_sub(self):
+        self.assertEqual(ao.subNum(3, 2), 1)
+     def test_mult(self):
+        self.assertEqual(ao.multNum(3, 2), 6)
+     def test_div(self):
+        self.assertEqual(ao.divNum(4, 2), 2)
+     def test_mod(self):
+        self.assertEqual(ao.modNum(4, 2), 0)
+     def test_floor(self):
+        self.assertEqual(ao.floorNum(4, 2), 2)
+     def test_exp(self):
+        self.assertEqual(ao.expNum(4, 2), 16)
+
+if __name__ == '__main__':
+    ut.main()
